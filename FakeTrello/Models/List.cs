f2076ec705e12c39b1368c2660cdee916e6f178e
Jennifer Dutton <jduttondesign@gmail.com>
2017-02-28 21:34:32 -0600

    first commit

---
+++ FakeTrello/Models/List.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Task { get; set; }
+
+
+    }
+}
---
+++ FakeTrello/Models/Users2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Users2
+    {
+        [Key]
+        public int Users2Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FullName { get; set; }
+
+    }
+}
