29cfebfb1af343e1b55bc40c42a3eea88a7d3966
Jennifer Dutton <jduttondesign@gmail.com>
2017-03-07 18:41:32 -0600

    removed card and user models

---
+++ FakeTrello/Models/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Card
+    {
+        [Key]
+        public int CardId { get; set; }
+
+        [MaxLength(60)]
+        public string Title { get; set; }
+
+        [MaxLength(60)]
+        public string Description { get; set; }
+
+        public List<Users2> Contributors { get; set; }//one to many
+        //public User2 Contributor {get; set;}//one to one user 
+
+        //does not link to any other card so no public list needed here.
+        //we provide navigation see below; we are saying return all columns not just the ID
+        //Auxiliary: given a card instance, return the list it belongs to. 
+        public List BelongsTo { get; set; }
+
+    }
+}
---
+++ FakeTrello/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class User//Author
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [MaxLength(60)]
+        public string UserName { get; set; }
+
+        [MaxLength(60)]
+        public string Email { get; set; }
+
+        [MaxLength(60)]
+        public string FullName { get; set; }
+
+        [MaxLength(60)]
+        public string PasswordHash { get; set; }
+
+        public List<Board> Boards { get; set; }//1 to many
+    }
+}
